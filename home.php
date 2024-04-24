1867c96307232a058834e4f9dc72df5f3d9c57a9
Arushi Sonal <130671061+Arushisonal@users.noreply.github.com>
2024-04-24 09:22:01 +0530

    added home.php

---
+++ home.php
@@ -0,0 +1,69 @@
+<?php 
+   session_start();
+
+   include("php/config.php");
+   if(!isset($_SESSION['valid'])){
+    header("Location: index.php");
+   }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Home</title>
+    <style>
+    body {
+            background-image: url('https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1200,h_630/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/p6l1zjdptfjx0i9woyxf/LIVE!%20Online%20tour%20-%20Travel%20around%20the%20world%20via%20screen%20in%2010%20countries%20with%20MyProGuide.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+        </style>
+</head>
+<body>
+    <div class="nav">
+        <div class="logo">
+            <p><a href="home.php">Tours & Travels</a> </p>
+        </div>
+
+        <div class="right-links">
+
+            <?php 
+            
+            $id = $_SESSION['id'];
+            $query = mysqli_query($con,"SELECT*FROM users WHERE Id=$id");
+
+            while($result = mysqli_fetch_assoc($query)){
+                $res_Uname = $result['Username'];
+                $res_Email = $result['Email'];
+                $res_id = $result['Id'];
+            }
+            
+            echo "<a href='register.php?Id=$res_id'>Change Profile</a>";
+            ?>
+
+            <a href="php/logout.php"> <button class="btn">Log Out</button> </a>
+
+        </div>
+    </div>
+    <main>
+
+       <div class="main-box top">
+          <div class="top">
+            <div class="box">
+                <p>Hello <b><?php echo $res_Uname ?></b>, Welcome</p>
+            </div>
+            <div class="box">
+                <p>Your email is <b><?php echo $res_Email ?></b>.</p>
+            </div>
+          </div>
+    
+          </div>
+       </div>
+
+    </main>
+</body>
+</html>
